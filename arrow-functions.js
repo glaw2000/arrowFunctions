46062bc8c2592a96dc85043cc4a319c2d87c9c28
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-10-17 13:27:32 +0000

    add mulit line syntax

--- arrow-functions.js
+++ arrow-functions.js
@@ -33,6 +33,16 @@ console.log(sum2);
 const saySomething = Message => console.log(Message);
 saySomething('Hello there!!');
 
-const sayHello = () => console.log('hello')
+const sayHello = () => console.log('hello');
 sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines());
+
